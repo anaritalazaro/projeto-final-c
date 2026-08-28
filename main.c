bc7156263021a0c05334dd8d5489efe5ba263c3e
AnaRitaSilva <4957229@formacao.iefp.pt>
2026-08-28 15:46:40 +0100

    [Sessão 3] Empate e reinicio do jogo

--- main.c
+++ main.c
@@ -10,27 +10,40 @@ int main(void)
     int jogadas = 0; // Contador de jogadas
     char jogador_atual = 'X';
 
-    char vencedor;
+    char vencedor='.';
+
 
     mostrar_boas_vindas();
 
-    show_menu();
-    scanf("%d", &opcao);
-    getchar();
-
-    init_board(board);
-
-    while (jogadas < 42) // máximo jogadas no jogo dos 4 em linha
+    do
     {
-        //Mostrar o tabuleiro
-        print_board(board);
-        //Anunciar de quem é o turno (uma linha, com %c)
-        printf("\n--- Turno do jogador %c ---\n", jogador_atual);
-        // Pedir a coluna com get_move
-        get_move(&col);
-        // Largar a peça (drop_piece) com essa coluna e o jogador_atual , guardando o retorno em 'linha'
-        linha = drop_piece(board, col, jogador_atual);
-        // Se devolveu -1, avisar que a coluna está cheia e voltar atrás sem gastar a jogada
+        show_menu();
+        scanf("%d", &opcao);
+        getchar();
+
+        if (opcao == 2)
+        {
+            break;
+        }
+
+        //reiniciar tabuleiro
+        init_board(board);
+
+        jogadas = 0;
+        jogador_atual = 'X';
+        vencedor = '.';
+
+        while (jogadas < 42) // máximo jogadas no jogo dos 4 em linha
+        {
+            //Mostrar o tabuleiro
+            print_board(board);
+            //Anunciar de quem é o turno (uma linha, com %c)
+            printf("\n--- Turno do jogador %c ---\n", jogador_atual);
+            // Pedir a coluna com get_move
+            get_move(&col);
+            // Largar a peça (drop_piece) com essa coluna e o jogador_atual , guardando o retorno em 'linha'
+            linha = drop_piece(board, col, jogador_atual);
+            // Se devolveu -1, avisar que a coluna está cheia e voltar atrás sem gastar a jogada
                 //Ajuda Formador Rúben
                 if (linha == -1) 
                 {
@@ -57,8 +70,16 @@ int main(void)
                 {
                     jogador_atual = 'X';
                 }
+        }
+        print_board(board);
+
+
+        if (vencedor == '.')
+        {
+            printf("\nEmpate! O tabuleiro esta cheio.\n");
+        }
     }
-    print_board(board);
+    while(1);
 
     return 0;
 }
